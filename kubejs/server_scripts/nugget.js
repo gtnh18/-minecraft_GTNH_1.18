f16f8d5a6b1e7703914f01f58e7da03c72559685
BuildTools <unconfigured@null.spigotmc.org>
2023-02-10 13:29:46 +0900

    cogwheel

--- kubejs/server_scripts/nugget.js
+++ kubejs/server_scripts/nugget.js
@@ -1,5 +1,5 @@
 onEvent('recipes', event => {
-	event.remove({output:"'#forge:nuggets/bronze'"});
+	event.remove({output:"#forge:nuggets/bronze"});
     //event.shapeless is a shortcut for event.recipes.minecraft.crafting_shapeless
     event.shapeless(
         Item.of('antimatter_shared:nugget_bronze', 1), // arg 1: output
